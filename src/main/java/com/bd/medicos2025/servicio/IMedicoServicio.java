ccde673410ad73911607576c25e4042c38f8396f
DannyVess <maverick2015@hotmail.com>
2025-02-20 02:43:55 -0500

    Examen final terminado

--- src/main/java/com/bd/medicos2025/servicio/IMedicoServicio.java
+++ src/main/java/com/bd/medicos2025/servicio/IMedicoServicio.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.bd.medicos2025.modelo.TblMedico;
 
 public interface IMedicoServicio {
-	//declaramos los metodos
+	//declaramos los metodos...
 	void RegistrarMedico(TblMedico tblmedico);
 	void EliminarMedico(TblMedico tblmedico);
 	List<TblMedico> ListadoMedicos();
